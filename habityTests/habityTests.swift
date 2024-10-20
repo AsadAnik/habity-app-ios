e78587d8354812fe1e298535fc38eee7a345cf8b
asadanik <s.engineer63@gmail.com>
2024-10-21 00:01:19 +0600

    Initial Commit

---
+++ habityTests/habityTests.swift
@@ -0,0 +1,17 @@
+//
+//  habityTests.swift
+//  habityTests
+//
+//  Created by Asad Anik on 10/21/24.
+//
+
+import Testing
+@testable import habity
+
+struct habityTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
